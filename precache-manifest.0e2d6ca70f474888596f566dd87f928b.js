0dbf9f4d46a0189f04b921609ff4bcb3152539ea
Manas Suri <manas.suri@ad.infosys.com>
2019-08-15 18:27:47 +0530

    Updates

---
+++ precache-manifest.0e2d6ca70f474888596f566dd87f928b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hubsearch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d505109b6927d4edebdc",
+    "url": "/hubsearch/static/js/runtime~main.d505109b.js"
+  },
+  {
+    "revision": "ffec9547bf897b0ba8be",
+    "url": "/hubsearch/static/js/main.ffec9547.chunk.js"
+  },
+  {
+    "revision": "8921ab57fc73e2575552",
+    "url": "/hubsearch/static/js/2.8921ab57.chunk.js"
+  },
+  {
+    "revision": "ffec9547bf897b0ba8be",
+    "url": "/hubsearch/static/css/main.8cf18aae.chunk.css"
+  },
+  {
+    "revision": "c1d7dd6ce88653708b2074f3fd031622",
+    "url": "/hubsearch/index.html"
+  }
+];
